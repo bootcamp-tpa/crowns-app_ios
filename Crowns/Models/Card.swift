3b93adacf6c625e7df824dcf05fabe8bcac36c97
Damian Kolasinski <damian.kolasinski@appunite.com>
2019-06-11 16:18:42 +0200

    Added remaining API models

--- Crowns/Models/Card.swift
+++ Crowns/Models/Card.swift
@@ -1,9 +1,24 @@
-struct Card: Decodable {
-    enum `Type`: String, Decodable {
+struct Card: Codable {
+    enum `Type`: String, Codable {
         case event = "EVENT"
         case death = "DEATH"
     }
     
+    enum Image: String, Codable {
+        case church = "CHURCH"
+        case church2 = "CHURCH_2"
+        case etc = "ETC"
+        case etc2 = "ETC_2"
+        case misc = "MISC"
+        case misc2 = "MISC_2"
+        case treasury = "TREASURY"
+        case treasury2 = "TREASURY_2"
+    }
+    
+    let id: Int
     let cardText: String
     let cardType: Type
+    let cardImage: Image
+    let leftChoice: CardChoice
+    let rightChoice: CardChoice
 }
